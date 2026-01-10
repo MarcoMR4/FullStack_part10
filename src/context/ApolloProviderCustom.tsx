24dfeaae8982016c1f8c50c2ef9c6e696524831e
Marco MR <marcomr9641@gmail.com>
2026-01-10 15:05:40 -0600

    refactor: remove ApolloProviderCustom component as part of code cleanup

--- src/context/ApolloProviderCustom.tsx
+++
@@ -1,9 +0,0 @@
-import createApolloClient from '@/src/utils/apolloClient';
-import { ApolloProvider } from '@apollo/client/react';
-
-const apolloClient = createApolloClient();
-
-// Wraps children with ApolloProvider
-export function ApolloProviderCustom({ children }: { children: React.ReactNode }) {
-  return <ApolloProvider client={apolloClient}>{children}</ApolloProvider>;
-}
